29f4cef890ee716e29ca7f2488e16132fc261535
labr0110 <labr0110@algonquinlive.com>
2018-01-20 11:29:37 -0500

    fixed image loader

--- js/image-loader.js
+++ js/image-loader.js
@@ -1,11 +1,8 @@
 window.addEventListener('load', function (e) {
-  var imgs = document.querySelectorAll('.non-critical-img');
+  var imgs = document.querySelectorAll('[data-non-critical-src]');
 
   [].forEach.call(imgs, function (img) {
-    var imgTag = img.querySelector('img');
-
-    imgTag.src = imgTag.dataset.src;
-    imgTag.removeAttribute('hidden');
+    img.src = img.dataset.nonCriticalSrc;
     img.removeAttribute('hidden');
   });
 });
